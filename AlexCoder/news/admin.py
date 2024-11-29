97262974090c39e9b5321fd78a15ce7540476b4c
zerocoderalex <alex12449@mail.ru>
2024-11-29 19:51:33 +0500

    rm cache

--- AlexCoder/news/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import News_post
-# Register your models here.
-admin.site.register(News_post)
-
--- AlexCoder/news/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import News_post
-# Create your views here.
-def home(request):
-	news = News_post.objects.all()
-	return render(request, 'news/news.html', {'news': news})
